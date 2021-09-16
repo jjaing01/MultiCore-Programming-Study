01719f7364576bd03f9f45456a49b22bb4ab4db5
Lee Dong Hee <jjaing01@naver.com>
2021-09-16 18:55:10 +0900

    <test> Add Test Code 1 - mutex lock
    - DataRace를 방지하기 위해 코드에 lock을 사용
    - 쓰레드 개수 증가에 따른 실행 시간 측정
    - 쓰레드 개수가 늘어나도 효율성은 증가하지 않고 오히려 성능이 저하됨

--- MultiCore Programming Strudy/00.Efficiencyn umber of Thread.cpp
+++ MultiCore Programming Strudy/00.Efficiencyn umber of Thread.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <mutex>
 
 using namespace std;
+using namespace chrono;
 
+// 1¾ï
+#define N 50'000'000
+#define CORE 8
+
+mutex my_l;
+int sum = 0;
+void worker(const int& number)
+{
+	for (auto i = 0; i < number; ++i)
+	{
+		my_l.lock();
+		sum += 2;
+		my_l.unlock();
+	}
+}
 int main()
 {
+	for (int i = 1; i <= CORE; i *= 2)
+	{
+		sum = 0;
+		int div = N / i;
+
+		vector<thread> workers;
+		auto start_t = high_resolution_clock::now();
+		for (int j = 0; j < i; ++j)
+			workers.emplace_back(worker, div);
+		for (auto& th : workers)
+			th.join();
+		auto end_t = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(end_t - start_t).count();
+		
+		cout << "-------------------------------------" << endl;
+		cout << "number of threads = " << i << endl;
+		cout << "exec time = " << duration << "ms" << endl;
+		cout << "sum = " << sum << endl;
+	}
 	return 0;
 }

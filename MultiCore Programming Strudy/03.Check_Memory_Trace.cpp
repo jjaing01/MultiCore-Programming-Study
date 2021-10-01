5e965f91159b4b731da7b5d90a581540e18f28fa
Lee Dong Hee <jjaing01@naver.com>
2021-10-01 20:51:06 +0900

    <test> 멀티쓰레드에서 메모리 일관성 체크
    1. 각 쓰레드에서 x,y값을 각각 증가시키면서 이웃 쓰레드의 값도 기록하여
       메모리 일관성을 확인해보았다.

---
+++ MultiCore Programming Strudy/03.Check_Memory_Trace.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <atomic>
+
+using namespace std;
+
+const int SIZE = 50'000'000;
+volatile int x, y;
+int trace_x[SIZE];
+int trace_y[SIZE];
+
+void worker1()
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		x = i;
+		trace_y[i] = y;
+	}
+}
+void worker2()
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		y = i;
+		trace_x[i] = x;
+	}
+}
+
+int main()
+{
+	thread t1{ worker1 };
+	thread t2{ worker2 };
+	t1.join();
+	t2.join();
+
+	int iCnt = 0;
+	for (int i = 0; i < SIZE - 1; ++i) // i == y
+	{
+		// 연속된 값이 아니라면 검사 X
+		if (trace_x[i] != trace_x[i + 1]) continue;
+
+		int x_value = trace_x[i];
+		if (trace_y[x_value] != trace_y[x_value + 1]) continue;
+
+		if (i != trace_y[x_value]) continue;
+
+		//cout << "x =" << x_value << ", y =" << trace_y[x_value] << endl;
+		iCnt++;
+	}
+
+	cout << "Memory Consistency Violation: " << iCnt << endl;
+
+	return 0;
+}
